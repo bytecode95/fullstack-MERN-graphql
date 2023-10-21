de47282ab5a59941f36e18d715e957cffc279b95
chethanavirajini <cheth.virajini@gmail.com>
2023-10-22 00:02:05 +0530

    sets up a basic Express server with graphQL

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 require('dotenv').config();
+const { createHandler } = require('graphql-http/lib/use/express') ;
+const schema = require('../Server/schema/schema');
+
 
 const port = process.env.PORT || 5000;
 
 
+// Create a express instance serving all methods on `/graphql`
+// where the GraphQL over HTTP express request handler is
 const app = express();
+app.all('/graphql', createHandler({ schema }));
+
 
 
 app.listen(port, console.log(`Server running on port ${port} `));
